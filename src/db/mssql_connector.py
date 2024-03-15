edf3c75a5dd18b14542c27fbc9380cfb9cb608f5
brunorodalvs <brunorodalvs@gmail.com>
2024-03-15 18:41:17 -0300

    Adicionado nova query

--- src/db/mssql_connector.py
+++ src/db/mssql_connector.py
@@ -13,7 +13,7 @@ class MottuDatabase:
         self.conn = pymssql.connect(
             server = sqlServer,
             user = sqlUSer,
-            psw = sqlPassword,
+            password = sqlPassword,
             database = sqlDatabase
         )
 
@@ -22,30 +22,102 @@ class MottuDatabase:
         with self.conn.cursor() as cursor:
             cursor.execute(
                 f"""
-                SELECT 
-                    UE.id,
-                    UE.usuarioId,
-                    U.nome,
-                    CASE WHEN U.matriz = 1 THEN U.id ELSE U.usuarioMatrizId END AS usuarioMatrizId,
-                    UE.residenciaLatitude,
-                    UE.residenciaLongitude,
-                    UE.residenciaCidade,
-                    UE.residenciaEstado,
-                    UE.residenciaBairro,
-                    U.marketplaceTipoCobranca
+        DECLARE @SLA_TMA_Standard DECIMAL(5, 2) = 90.00;
+        DECLARE @SLA_ChegadaNaColeta_Standard DECIMAL(5, 2) = 90.00;
+        DECLARE @TaxaDeCancelamentoStandard DECIMAL(5, 2) = 0.00;
+        DECLARE @DataInicio DATE = DATEADD(DAY, -7, GETUTCDATE());
+        declare @matrizID int = 243081;
 
-                FROM usuarioEndereco UE
-                JOIN usuario U ON UE.usuarioId = U.id
-                WHERE 1=1
-                    AND U.perfilId IN (7,29)
-                    AND U.marketplaceTipoCobranca = 20
+        ;WITH EventosOrdenados AS (
+            -- Primeira CTE para ordenar eventos por criacaoData
+            SELECT
+                pedidoId,
+                pedidoEventoTipoId,
+                criacaoData,
+                ROW_NUMBER() OVER(PARTITION BY pedidoId, pedidoEventoTipoId ORDER BY criacaoData DESC) AS Rn
+            FROM pedidoEvento
+            WHERE criacaoData >= @DataInicio
+        ), EventosAgregados AS (
+            -- Segunda CTE para agregar os dados da primeira CTE
+            SELECT
+                EO.pedidoId,
+        		P.usuarioOrigemId,
+        		MAX(CASE WHEN pedidoEventoTipoId = 5 AND Rn = 1 THEN EO.criacaoData END) AS criacaodata,
+                MAX(CASE WHEN pedidoEventoTipoId = 1 AND Rn = 1 THEN EO.criacaoData END) AS dataAceito,
+                MAX(CASE WHEN pedidoEventoTipoId = 3 AND Rn = 1 THEN EO.criacaoData END) AS dataEntregue,
+                MIN(CASE WHEN pedidoEventoTipoId = 10 AND Rn = 1 THEN EO.criacaoData END) AS dataChegadaColeta,
+                MAX(CASE WHEN pedidoEventoTipoId = 2 AND Rn = 1 THEN EO.criacaoData END) AS dataRetirada,
+                MAX(CASE WHEN pedidoEventoTipoId = 11 AND Rn = 1 THEN EO.criacaoData END) AS dataChegadanaEntrega,
+        		MAX(CASE WHEN pedidoEventoTipoId = 21 AND Rn = 1 THEN EO.criacaoData END) AS dataInicioRetorno,
+        		MAX(CASE WHEN pedidoEventoTipoId = 19 AND Rn = 1 THEN EO.criacaoData END) AS dataEntregueRetorno,
+        		MAX(CASE WHEN pedidoEventoTipoId in (3,6,8,19) AND Rn = 1 THEN EO.criacaoData END) AS dataFim,
+        		MAX(CASE WHEN pedidoEventoTipoId = 18 AND Rn = 1 THEN EO.criacaoData END) AS dataPrevisto,
+                COUNT(CASE WHEN pedidoEventoTipoId = 7 AND Rn = 1 THEN 1 END) AS Kicks_totais,
+                COUNT(CASE WHEN pedidoEventoTipoId = 14 AND Rn = 1 THEN 1 END) AS Desistencias,
+        		P.situacao as pedidosituacao,
+        		count(CASE WHEN pedidoEventoTipoId in (3,8,19) AND Rn = 1 THEN 1 END) as Entregas
 
-                    ORDER BY 1 
-                """
+            FROM EventosOrdenados EO inner join pedido P on P.id = EO.pedidoid
+        	left join PedidoEntrega PE on PE.pedidoid = P.id
+        	where P.pedidotipoid != 3
+        	and P.Origem != 'mottu'
+            GROUP BY EO.pedidoId,P.usuarioOrigemId,P.situacao
+        ), AnaliseUsuarios as (
+        SELECT
+            U.id as usuarioid, 
+        	U.nome,
+        	count(EA.criacaoData) as PedidosTotais,
+        	sum(EA.Entregas) as Entregas,
+        	COUNT(CASE WHEN datediff(minute,EA.criacaoData,dataChegadaColeta)<=15 THEN 1 ELSE NULL END) AS PedidosDentrodoSLA,
+        	COUNT(CASE WHEN EA.pedidosituacao in (200,50,70,171) THEN 1 ELSE NULL END) AS PedidosCanceladosTotal,
+        	COUNT(CASE WHEN EA.pedidosituacao in (200,50,70,171) and datediff(minute,EA.criacaoData,dataFim)>=30 and dataPrevisto is null THEN 1 ELSE NULL END) AS PedidosCancelados,
+        	COUNT(CASE WHEN EA.pedidosituacao in (171) THEN 1 ELSE NULL END) AS PedidosExtraviados,
+        	COUNT(CASE WHEN EA.pedidosituacao in (30) and datediff(minute,EA.criacaoData,dataFim)<=60 and dataPrevisto is null THEN 1 ELSE NULL END) AS PedidosEntreguesTMA,
+        	max(UE.residenciaLatitude) as residenciaLatitude,
+        	max(UE.residenciaLongitude) as residenciaLongitude,
+        	max(Ue.distritoH3Resolucao7) as distritoH3Resolucao7,
+			UE.residenciaBairro,
+			UE.residenciaCidade
+
+
+        FROM EventosAgregados EA 
+        	inner join usuario U on EA.usuarioOrigemId = U.id 
+        	left join usuarioEndereco UE on U.id = UE.usuarioId
+
+        where EA.dataAceito >= @DataInicio
+        
+        	and U.perfilId in (7,29)
+
+
+        group by U.id, U.nome, UE.residenciaBairro,
+			UE.residenciaCidade),
+        PrincipaisIndicadores as (
+        
+        select nome,
+        	usuarioid,
+        	residenciaLatitude,
+        	residenciaLongitude,
+			residenciaCidade,
+			residenciaBairro,
+        	
+        	PedidosTotais,
+        	PedidosDentrodoSLA ,
+        	PedidosEntreguesTMA,
+        	PedidosCanceladosTotal,
+        	PedidosCancelados,
+        	PedidosExtraviados
+        	from AnaliseUsuarios AU
+         )
+
+         select * 
+        	from PrincipaisIndicadores
+
+        order by 2 desc
+        """
             )
             
-            dtcolumns = ['id','usuarioId','nome','usuarioMatrizId','Latitude','Longitude'
-                         , 'Cidade','Estado','Bairro', 'tipoCobranca'
+            dtcolumns = ['nome','usuarioId','latitude','longitude','cidade','bairro','pedidosTotais','pedidosDentroDoSLA'
+                         , 'pedidosEntreguesTMA','pedidosCanceladosTotal','pedidosCancelados', 'pedidosExtraviados'
                          ]
 
             return pd.DataFrame(cursor.fetchall(), columns=dtcolumns)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,28 @@
-from utils.request_maker import get_token
+from db.mssql_connector import MottuDatabase
+import h3
+import pandas as pd
 
-token = get_token()
-print(token)
+
+sql = MottuDatabase()
+
+usuarioResidencia = sql.usuarioResidencia()
+listCompanies = []
+i = 0
+for company in usuarioResidencia['usuarioId']:
+    company = dict(
+        usuarioId = usuarioResidencia['usuarioId'][i],
+        nome = usuarioResidencia['nome'][i],
+        bairro = usuarioResidencia['bairro'][i],
+        cidade = usuarioResidencia['cidade'][i],
+        Latitude = usuarioResidencia['latitude'][i],
+        Longitude = usuarioResidencia['longitude'][i],
+        H3_7 = h3.geo_to_h3(
+            usuarioResidencia['latitude'][i],
+            usuarioResidencia['longitude'][i], 7 )
+    )
+    i+=1
+    listCompanies.append(company)
+
+df = pd.DataFrame(listCompanies).sort_values(by=['H3_7'])
+print(df)
+# df.to_excel('clientes_h3.xlsx',sheet_name="main")
